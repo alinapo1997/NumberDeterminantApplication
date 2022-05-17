3cfef8078dd09a79e72066c5744b325f8cc95673
Alina Podvalnaia <alinapo1997@gmail.com>
2022-05-18 00:16:29 +0400

    Добавьте файлы проекта.

---
+++ NumberDeterminantApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NumberDeterminantApplication
+{
+    /// <summary>
+    /// Приложение по определению чётного или нечётного числа
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Чётное или нечётное?");
+            Console.WriteLine("Введите целое число:");
+            int intValue = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            string result = intValue % 2 == 0 ? " - чётное число" : " - нечётное число";
+            Console.WriteLine(intValue + result);
+            Console.ReadKey();
+        }
+    }
+}
